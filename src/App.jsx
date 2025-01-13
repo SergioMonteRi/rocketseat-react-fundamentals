e194b905b0dec3da838d0e558c1a539a8e3f6375
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-01-13 17:24:28 -0300

    create components folder and testing css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { Header } from "./components/Header"
+
 function App() {
   return (
-    <h1>React App</h1>
+    <Header />
   )
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+
+
+import styles from './styles.module.css';
+
+export const Header = () => {
+    return (
+        <header className={styles.header}>
+            <strong>Ignite Feed</strong>
+        </header>
+    )
+}
